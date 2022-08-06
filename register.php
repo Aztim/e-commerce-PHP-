4cd571b019d9840534779b54916df45ad08122b3
Aztim <barboza-82@mail.ru>
2022-08-06 20:32:17 +0300

    added database into register.php

--- register.php
+++ register.php
@@ -1,9 +1,39 @@
 <?php
+
+include('./server/connection.php');
+
 if(isset($_POST['register'])){
   $name = $_POST['name'],
   $email = $_POST['email'],
   $password = $_POST['password '],
   $confirmPassword = $_POST['confirmPassword']
+
+  //if passwords dont match
+  if($password !== $confirmPassword){
+    header('location: register.php?error=passwords dont match');
+  }
+
+   //if passwords less then 6 char
+  if(strlen($password) < 6){
+    header('location: register.php?error=passwords must be at least 6 charachters');
+  }
+  //check whether there is a user with this email or not
+  $stmt1= $conn->prepare("SELECT count(*) FROM users where email=?");
+  $stmt1->bind_param('s', $email);
+  $stmt1->execute();
+  $stmt1->bind_result($num_rows);
+  $stmt1->fetch();
+
+  if($num_rows !=0){
+    header('location: register.php?error=user with this email already exists');
+  }
+
+  //create a new user
+  $stmt = $conn->prepare("INSERT INTO users (user_name,user_email,user_password)
+                  VALUES (?,?,?)");
+
+  $stmt->bind_param('sss',$name,$email,md5($password));
+
 
 }else{
 
@@ -72,6 +102,7 @@ if(isset($_POST['register'])){
       </div>
       <div class="mx-auto container">
         <form id="register-form" method="POST" action="register.php">
+          <p style="color: red;"><?php echo $_GET['error']; ?></p>
           <div class="form-group">
             <label for="">Name</label>
             <input type="text" class="form-control" id="register-name" name="name" placeholder="Name" required />
