e77bb34b9a9896c54ff434352a7049a792a5167a
Aztim <barboza-82@mail.ru>
2022-08-07 22:35:30 +0300

    finished register.php

--- register.php
+++ register.php
@@ -11,32 +11,49 @@ if(isset($_POST['register'])){
   //if passwords dont match
   if($password !== $confirmPassword){
     header('location: register.php?error=passwords dont match');
-  }
 
    //if passwords less then 6 char
-  if(strlen($password) < 6){
+  }else if(strlen($password) < 6){
     header('location: register.php?error=passwords must be at least 6 charachters');
+
+
+    // if there is no error
+  }else{
+    //check whether there is a user with this email or not
+    $stmt1= $conn->prepare("SELECT count(*) FROM users where user_email=?");
+    $stmt1->bind_param('s', $email);
+    $stmt1->execute();
+    $stmt1->bind_result($num_rows);
+    $stmt1->fetch();
+
+    //if there is a user with already registered with this email
+    if($num_rows !=0){
+      header('location: register.php?error=user with this email already exists');
+
+    //if no user registed with this email before
+    }else{
+
+      //create a new user
+      $stmt = $conn->prepare("INSERT INTO users (user_name,user_email,user_password)
+                      VALUES (?,?,?)");
+
+      $stmt->bind_param('sss',$name,$email,md5($password));
+
+      if($stmt->execute()){
+        $_SESSION['user_email'] = $email;
+        $_SESSION['user_name'] = $name;
+        $_SESSION['logged-in'] = true;
+        header('location: account.php?register=You registered successfully');
+
+        //account could not be created
+      }else{
+        header('location: register.php?error=could not create an account at the moment')
+      }
+    }
   }
-  //check whether there is a user with this email or not
-  $stmt1= $conn->prepare("SELECT count(*) FROM users where email=?");
-  $stmt1->bind_param('s', $email);
-  $stmt1->execute();
-  $stmt1->bind_result($num_rows);
-  $stmt1->fetch();
-
-  if($num_rows !=0){
-    header('location: register.php?error=user with this email already exists');
-  }
-
-  //create a new user
-  $stmt = $conn->prepare("INSERT INTO users (user_name,user_email,user_password)
-                  VALUES (?,?,?)");
-
-  $stmt->bind_param('sss',$name,$email,md5($password));
-
 
 }else{
-
+  header('location: register.php?error=please fill in the form');
 }
 
 ?>
@@ -102,7 +119,7 @@ if(isset($_POST['register'])){
       </div>
       <div class="mx-auto container">
         <form id="register-form" method="POST" action="register.php">
-          <p style="color: red;"><?php echo $_GET['error']; ?></p>
+          <p style="color: red;"><?php if(isset($_GET['error'])) {echo $_GET['error']; }?></p>
           <div class="form-group">
             <label for="">Name</label>
             <input type="text" class="form-control" id="register-name" name="name" placeholder="Name" required />
