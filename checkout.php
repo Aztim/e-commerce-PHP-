f80e10fbc29262109daa11d3e0da983fa677669f
Aztim <barboza-82@mail.ru>
2022-08-16 16:24:11 +0300

    fixed header-and footer components

--- checkout.php
+++ checkout.php
@@ -3,9 +3,8 @@
 session_start();
 
 
-if( !empty($_SESSION['cart']) && isset($_POST)) {
+if(!empty($_SESSION['cart'])) {
   //let user in
-
 
   //send user to home page
 }else{
@@ -24,6 +23,14 @@ if( !empty($_SESSION['cart']) && isset($_POST)) {
     </div>
     <div class="mx-auto container">
       <form id="checkout-form" method="POST" action="server/place_order.php">
+
+      <p class="text-center" style="color: red;">
+        <?php if(isset($_GET['message'])){ echo $_GET['message']; } ?>
+        <?php if(isset($_GET['message'])) {?>
+          <a href="login.php" class="btn btn-primary">Login</a>
+        <?php } ?>
+      </p>
+
         <div class="form-group checkout-small-element">
           <label for="">Name</label>
           <input type="text" class="form-control" id="checkout-name" name="name" placeholder="Name" required />
--- layouts/header.php
+++ layouts/header.php
@@ -33,12 +33,12 @@
           </li>
 
           <li class="nav-item">
-            <a class="nav-link" href="contact.html">Contact Us</a>
+            <a class="nav-link" href="contact.php">Contact Us</a>
           </li>
 
           <li class="nav-item icons">
             <a href="cart.php"><i class="fa fa-shopping-bag" aria-hidden="true"></i></a>
-            <a href="account.html"><i class="fa fa-user" aria-hidden="true"></i></a>
+            <a href="account.php"><i class="fa fa-user" aria-hidden="true"></i></a>
           </li>
         </ul>
 
