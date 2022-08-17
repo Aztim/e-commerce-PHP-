29f115301360c962cd1a73a76ddc547d3b59842f
Aztim <barboza-82@mail.ru>
2022-08-17 20:54:30 +0300

    intagrated with paypal

--- payment.php
+++ payment.php
@@ -21,17 +21,64 @@ if( isset($_POST['order_pay_btn'])) {
 
       <?php if(isset($_SESSION['total']) && $_SESSION['total'] != 0) {?>
           <p>Total payment: $<?php echo $_SESSION['total']; ?></p>
-          <input class="btn btn-primary" type="submit" value="Pay Now"/>
+          <!-- <input class="btn btn-primary" type="submit" value="Pay Now"/> -->
+
+          <!-- Set up a container element for the button -->
+          <div id="paypal-button-container"></div>
 
       <?php } else if(isset($_POST['order_status']) && $_POST['order_status'] == "not paid") { ?>
           <p>Total payment: $<?php echo $_POST['order_total_price']; ?></p>
-          <input class="btn btn-primary" type="submit" value="Pay Now"/>
+          <!-- <input class="btn btn-primary" type="submit" value="Pay Now"/> -->
+            <!-- Set up a container element for the button -->
+          <div id="paypal-button-container"></div>
 
       <?php } else  { ?>
           <p>You dont have an order</p>
       <?php } ?>
 
+
+
+
+
     </div>
   </section>
 
-  <?php include('layouts/footer.php') ?>
+
+   <!-- Include the PayPal JavaScript SDK; replace "test" with your own sandbox Business account app client ID -->
+   <script src="https://www.paypal.com/sdk/js?&client-id=<ARtmn2Vfi4UjyFjDmqtu4DKU0NUFrUkpulKLg0gvyYBcUGY5LYNUK-h0a5vexN08kWZOh8TCTAvZ4eTF>&merchant-id=<Merchant-ID>&currency=USD"></script>
+
+
+<script>
+  paypal.Buttons({
+
+    // Sets up the transaction when a payment button is clicked
+    createOrder: function(data, actions) {
+      return actions.order.create({
+        purchase_units: [{
+          amount: {
+            value: '77.44' // Can reference variables or functions. Example: `value: document.getElementById('...').value`
+          }
+        }]
+      });
+    },
+
+    // Finalize the transaction after payer approval
+    onApprove: function(data, actions) {
+      return actions.order.capture().then(function(orderData) {
+        // Successful capture! For dev/demo purposes:
+            console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
+            var transaction = orderData.purchase_units[0].payments.captures[0];
+            alert('Transaction '+ transaction.status + ': ' + transaction.id + '\n\nSee console for all available details');
+
+        // When ready to go live, remove the alert and show a success message within this page. For example:
+        // var element = document.getElementById('paypal-button-container');
+        // element.innerHTML = '';
+        // element.innerHTML = '<h3>Thank you for your payment!</h3>';
+        // Or go to another URL:  actions.redirect('thank_you.html');
+      });
+    }
+  }).render('#paypal-button-container');
+
+</script>
+
+<?php include('layouts/footer.php') ?>
